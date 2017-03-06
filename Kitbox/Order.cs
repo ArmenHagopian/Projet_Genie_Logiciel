1ecdeb66756c0a5458bf0bc513a8b12c2af9edac
ArmenHagopian <14040@ecam.be>
2017-03-06 18:48:52 +0100

    More code in Order.cs

--- Kitbox/Order.cs
+++ Kitbox/Order.cs
@@ -1,10 +1,29 @@
 ﻿using System;
+using System.Collections.Generic;
 namespace Kitbox
 {
 	public class Order
 	{
+		protected Person current_client;
+		protected List<Wardrobe> wardrobes;
+		protected Dictionary<string, object> bill;
+		protected List<string> parts_list;
+		protected Dictionary<string, string> codes;
 		public Order()
 		{
+		}
+
+		private string AddToBill()
+		{
+			
+		}
+		private List<string> AddToPartsList()
+		{
+			
+		}
+		public void PrintAll()
+		{
+			
 		}
 	}
 }
