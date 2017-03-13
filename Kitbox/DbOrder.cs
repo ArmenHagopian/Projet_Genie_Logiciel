eef4f874d52134dbb235d5547302f42737cab208
ArmenHagopian <14040@ecam.be>
2017-03-13 10:25:34 +0100

    DbOrder.cs added

---
+++ Kitbox/DbOrder.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+namespace Kitbox
+{
+	public class DbOrder
+	{
+		public DbOrder()
+		{
+		}
+		public void DbConnectOrder(string[] parameters)
+		{
+			
+		}
+		public void DbAddOrder(Order order)
+		{
+
+		}
+		public Dictionary<string, object> DbSearchClient(int id_client)
+		{
+
+		}
+	}
+}
